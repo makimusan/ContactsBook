ec666d3cafcaca1559eba91e8eec9a89ab735c68
makimu <makimu@tut.by>
2020-04-30 00:15:16 +0300

    Has been added new project "Locator"

---
+++ ContactsBook.Design/Services/IView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsBook.Design.Services
+{
+    public interface IView
+    {
+        object DataContext { get; set; }
+
+        void Show();
+    }
+}
--- ContactsBook/App.xaml.cs
+++ ContactsBook/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ContactsBook.Design.Views;
 
 namespace ContactsBook
 {
@@ -13,5 +14,11 @@ namespace ContactsBook
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            ContactsView view = new ContactsView();
+            //view.DataContext = new ViewModels.MainViewModel();
+            view.Show();
+        }
     }
 }
